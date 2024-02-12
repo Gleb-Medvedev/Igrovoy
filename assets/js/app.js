5a63dbf41688d899d15c599be220b1e5ce42876b
Gleb-Medvedev <4e4enell@gmail.com>
2024-02-12 23:08:34 +0300

    added approximate positioning of interactive header elements

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,8 @@
-document.querySelector('.menu-btn').addEventListener('click', () => {
-    document.querySelector('.menu-btn').classList.toggle('opened');
+// document.querySelector('.menu-btn').addEventListener('click', () => {
+//     document.querySelector('.menu-btn').classList.toggle('opened');
+// })
+
+document.querySelector('.bottom-bar__contact-us').addEventListener('click', () => {
+    document.querySelector('.bottom-bar__contact-us-list').classList.toggle('in-view');
 })
+
