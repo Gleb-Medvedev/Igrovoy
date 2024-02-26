06042d99a5f7ba14661783617f1d57b5e3ffd52d
Gleb-Medvedev <4e4enell@gmail.com>
2024-02-26 09:29:28 +0300

    added vendor prefixes, html template changes for contact button

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,7 @@
 const header = document.querySelector('.header');
 const main = document.querySelector('.main');
 
-// function headerInWiew() {
-//     window.addEventListener('scroll', () => {
-//         main.style.marginTop = header.offsetHeight + "px";
-
-//         if (window.scrollY >= header.offsetHeight) {
-//             header.classList.add('header_in-view');
-//             main.style.marginTop = header.offsetHeight + "px";
-//         } else {
-//             header.classList.remove('header_in-view');
-//         }
-//     })
-// }
-
-// headerInWiew();
-
-const swiper = new Swiper('.swiper', {
+const mainBannerSlider = new Swiper('.swiper', {
     direction: 'horizontal',
     loop: true,
     autoplay: true,
@@ -24,4 +9,21 @@ const swiper = new Swiper('.swiper', {
     spaceBetween: 20,
     allowTouchMove: false,
     speed: 1700,
+
+    navigation: {
+        nextEl: '.main-banner__headliner-next-btn',
+        prevEl: '.main-banner__headliner-prev-btn',
+      },
 });
+
+// document.querySelector('.bottom-bar__search-bar').addEventListener('click', () => {
+//   document.querySelector('.bottom-bar__search-bar').classList.toggle('active');
+// })
+
+// document.querySelector('.bottom-bar__menu-btn').addEventListener('click', () => {
+//   document.querySelector('.bottom-bar__menu-btn').classList.toggle('active');
+// })
+
+document.querySelector('.bottom-bar__feedback').addEventListener('click', () => {
+  document.querySelector('.bottom-bar__feedback').classList.toggle('active');
+})
