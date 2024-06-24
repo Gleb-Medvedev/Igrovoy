dd4caa6ea53833cf447a9abd2f3342f074f1da33
Gleb-Medvedev <4e4enell@gmail.com>
2024-06-24 23:23:50 +0300

    mnogo vsego

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,9 @@
-// const header = document.querySelector('.header');
-// const main = document.querySelector('.main');
+// Main Banner Slider
 
-const mainBannerSlider = new Swiper('.swiper', {
+const mainBannerSlider = new Swiper('.headliner-slider', {
     direction: 'horizontal',
     loop: true,
+    grabCursor: true,
     autoplay: {
       delay: 5000,
     },
@@ -27,7 +27,56 @@ const mainBannerSlider = new Swiper('.swiper', {
     },
 });
 
-{/* <svg width="26" height="26" viewBox="0 0 28 28"><circle class="svg__circle" cx="14" cy="14" r="12" stroke-width="2"></circle><circle class="svg__circle-inner" cx="14" cy="14" r="6" stroke-width="3"></circle></svg> */}
+// Advantages Slider
+
+const advantagesSlider = new Swiper('.advantages-slider', {
+  spaceBetween: 20,
+  loop: true,
+  speed: 2000,
+  grabCursor: true,
+  watchSlidesProgress: true,
+  autoplay: {
+    delay: 8000
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+      slidesPerGroup: 1,
+      autoplay: {
+        delay: 4000
+      },
+    },
+    580: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+      autoplay: {
+        delay: 8000
+      },
+    },
+    901: {
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+      autoplay: {
+        delay: 8000
+      },
+    },
+  },
+
+  on: {
+    slideChange: function () {
+      this.visibleSlides.forEach((slide, index) => {
+        slide.querySelector('.advantages__card_pseudo').style.animation =
+        `changePseudoWidth ${this.params.autoplay.delay}ms ease-out ${this.params.speed + (index * 400)}ms 1 forwards`;
+
+        slide.querySelector('.advantages__card').style.animation = 
+        `activeSlideBorderColor ${this.params.autoplay.delay}ms ease-in-out ${this.params.speed + (index * 400)}ms 1 forwards`;
+
+        slide.querySelector('.advantages__card-description').style.animation = 
+        `descriptionAppearance ${this.params.autoplay.delay}ms ease-out ${this.params.speed + (index * 400)}ms 1 forwards`;
+      })
+    },
+  },
+})
 
 //reseive data from local JSON Server.
 
@@ -35,22 +84,56 @@ const mainBannerSlider = new Swiper('.swiper', {
 
 let dataBase; //Глобальная переменная, хранящая данные
 
-async function getDataBase(url, reseivedData) {
-
-  try {
-    reseivedData = await fetch(url);
-  } catch (err) {
-    // alert(`Ошибка базы данных: ${err}\nДля запуска локального сервера используйте "npm run server"`);
-    return;
-  }
-
-  dataBase = await reseivedData.json();
+function getDataBase() {
+  document.addEventListener('DOMContentLoaded', async(url) => {
+    try {
+      url = await fetch('http://localhost:3000/products');
+    } catch(err) {
+      console.log(err);
+      return
+    }
+    dataBase = await url.json();
+    RenderLatestProducts(document.querySelector('.latest-products'));
+  })
 }
 
-document.addEventListener('DOMContentLoaded', async () => {
-  await getDataBase('http://localhost:3000/products');
-  dataBase ? console.log(dataBase) : console.log('Ошибка получения данных из базы');
-})
+getDataBase();
+
+//Latest Products Section 
+
+function RenderLatestProducts(destination, tmp) {
+  for (const product of dataBase) {
+    tmp = document.createElement('div');
+    tmp.classList.add('latest-products__item', 'product');
+
+    tmp.insertAdjacentHTML('beforeend', 
+      `
+      <div class="product-top">
+          <a href="#" class="product-top__img-link" title="${product.name}">
+              <img class="product-top__img thief-color" src="./assets/images/minecraft.jpg" alt="Изображение товара: ${product.name}" crossorigin="anonymous">
+          </a>    
+      </div>
+      <div class="product-body">
+          <a href="#" class="product-body__title-link" title="${product.name}">
+              <span class="product-body__title-inner">${product.name}</span>
+          </a>
+          <div class="product-info" title="Количество товара">
+              ${product.quantity >= 1 ? product.quantity.textContent = 'В наличии' : product.quantity.textContent = 'Нет в наличии!'}
+          </div>
+          <div class="product-body__btn-container" title="Добавить ${product.name} в корзину!">
+              <button class="product-body__btn-inner">В корзину!</button>
+          </div>
+      </div> 
+      `
+    )
+    destination.append(tmp);
+    // const test = tmp.querySelector('.thief-color');
+    // const wrapp = tmp.closest('.latest-products__item');
+    // console.log(test);
+    // console.log(wrapp);
+    // ChangeBg(test, wrapp);
+  }
+}
 
 //reseive data from local JSON Server -- ENDS
 
@@ -119,16 +202,16 @@ const focusBlock = document.querySelector('.focus-block');
 
 //ФУНКЦИЯ ВЫЗОВА HEADER CONTACTS
 
-function toggleHeaderContacts (list, listItems) {
+function toggleHeaderContacts(list, listItems) {
   list.classList.toggle('in-view');
 
   if (list.classList.contains('in-view')) {
     listItems.forEach((el, index) => {
-      el.style.transitionDelay = `${(index + 1) * 75}ms`
+      el.style.transitionDelay = `${(index + 1) * 75}ms`;
     })
   } else {
     listItems.forEach((el, index) => {
-      el.style.transitionDelay = `${(listItems.length - index) * 75}ms`
+      el.style.transitionDelay = `${(listItems.length - index) * 75}ms`;
     })
   }
 }
@@ -138,3 +221,21 @@ document.querySelector('.contacts-btn').addEventListener('click', () => {
 })
 
 //ФУНКЦИЯ ВЫЗОВА HEADER CONTACTS -- ENDS
+
+
+
+
+function ToggleHeaderAction(arr) {
+  arr.forEach(action => {
+    action.addEventListener('click', () => {
+      arr.forEach(el => {
+        if (el !== action) {
+          el.classList.remove('active');
+        }
+      })
+      action.classList.toggle('active');
+    })
+  })
+}
+
+ToggleHeaderAction(document.querySelectorAll('.header-action'));
--- assets/js/bg-changer.js
+++ assets/js/bg-changer.js
@@ -6,6 +6,7 @@ function changeBgColor(images) {
       console.log('Нет объектов для применения "Color-thief"');
       return;
    }
+
    images.forEach(async (image, R, G, B) => {
 
       await colorThief.getColor(image);
@@ -14,13 +15,55 @@ function changeBgColor(images) {
       G = colorThief.getColor(image)[1];
       B = colorThief.getColor(image)[2];
 
-      image.closest('div').style.backgroundColor = 
+      image.closest('.latest-products__item').style.backgroundColor = 
          'rgb(' + R + 
             ',' + G + 
             ',' + B + ')';
-   })
+   })   
 }
 
 document.addEventListener('DOMContentLoaded', () => {
    changeBgColor(document.querySelectorAll('.thief-color'));
 })
+
+// document.querySelector('.test').addEventListener('click', () => {
+//    changeBgColor(document.querySelectorAll('.thief-color'));
+//    console.log('Клик');
+// })
+
+
+
+
+
+// function ChangeBg(arr) {
+//    arr.forEach(async (image, R, G, B) => {
+//       await colorThief.getColor(image);
+
+//       R = colorThief.getColor(image)[0];
+//       G = colorThief.getColor(image)[1];
+//       B = colorThief.getColor(image)[2];
+
+//       image.closest('.latest-products__item').style.backgroundColor = 
+//             'rgb(' + R + 
+//                ',' + G + 
+//                ',' + B + ')';
+//    })
+// }
+
+
+
+
+
+// async function ChangeBg(img, wrapper, R, G, B) {
+//    await colorThief.getColor(img);
+   
+
+//    R = colorThief.getColor(img)[0];
+//    G = colorThief.getColor(img)[1];
+//    B = colorThief.getColor(img)[2];
+
+//    wrapper.style.backgroundColor = 
+//       'rgb(' + R + 
+//          ',' + G + 
+//          ',' + B + ')'; 
+// }
